346c270ea63dcabd9bf8a50286a752a9c364e34c
ishaan <66063951+ishaansingh22@users.noreply.github.com>
2024-06-12 09:27:30 -0400

    Update sys_config.py

--- src/configs/sys_config.py
+++ src/configs/sys_config.py
@@ -1,4 +1,3 @@
-
 # System Configuration
 
 system_config = {
@@ -7,14 +6,15 @@ system_config = {
     "dset": "cifar10",
     "dump_dir": "./expt_dump/",
     "dpath": "/data/unagi0/anakewat/imgs/",
+    "seed": 2,
     "device_ids": {
         "iid_dispfl": {"node_0": [2], "node_1": [3], "node_2": [4], "node_3": [5], "node_4": [2]},
         "iid_defkt": {"node_0": [4], "node_1": [4], "node_2": [4], "node_3": [4]},
         "non_iid": {"node_0": [0], "node_1": [1], "node_2": [2], "node_3": [3]}
     },
     "dataset_splits": {
-        "iid": {"num_clients": 4, "samples_per_client": 500},
-        "non_iid": {"num_clients": 8, "samples_per_client": 32}
+        "iid": {"samples_per_user": 500},
+        "non_iid": {"samples_per_user": 32}
     }
 }
 
