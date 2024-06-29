ee4dc39396612b1b694940370290bf9fc3f7088f
ishaan <66063951+ishaansingh22@users.noreply.github.com>
2024-06-28 23:29:28 -0400

    fix configs

--- src/configs/sys_config.py
+++ src/configs/sys_config.py
@@ -6,9 +6,6 @@ system_config = {
     "dump_dir": "./expt_dump/",
     "dpath": "/data/unagi0/anakewat/imgs/",
     "seed": 2,
-    "device_ids": {}
-    "dataset_splits": {
-        "iid": {"samples_per_user": 500},
-        "non_iid": {"samples_per_user": 32}
-    }
+    "device_ids": {"node_0": [5], "node_1": [5],"node_2": [5], "node_3": [2], "node_4": [2], "node_5": [3], "node_6": [3], "node_7": [3], "node_8": [3]},
+    "dataset_splits": {"samples_per_user": 500}, #iid, same structure for non_iid
 }
