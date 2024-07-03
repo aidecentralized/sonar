409d1cfd03c380dde464b38f131d9314b34fe38b
dasdebanna <dasdebanna8@gmail.com>
2024-07-04 04:55:52 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,29 +1,31 @@
+"""
+This module runs collaborative learning experiments using the Scheduler class.
+"""
+
 import argparse
-from scheduler import Scheduler
-import gc
-import torch
-import copy
 import logging
+
+from scheduler import Scheduler
 
 logging.getLogger("PIL").setLevel(logging.INFO)
 
-b_default = "./configs/algo_config.py"
-s_default = "./configs/sys_config.py"
+B_DEFAULT = "./configs/algo_config.py"
+S_DEFAULT = "./configs/sys_config.py"
 
 parser = argparse.ArgumentParser(description="Run collaborative learning experiments")
 parser.add_argument(
     "-b",
     nargs="?",
-    default=b_default,
+    default=B_DEFAULT,
     type=str,
-    help="filepath for benchmark config, default: {}".format(b_default),
+    help=f"filepath for benchmark config, default: {B_DEFAULT}",
 )
 parser.add_argument(
     "-s",
     nargs="?",
-    default=s_default,
+    default=S_DEFAULT,
     type=str,
-    help="filepath for system config, default: {}".format(s_default),
+    help=f"filepath for system config, default: {S_DEFAULT}",
 )
 
 args = parser.parse_args()
@@ -31,7 +33,6 @@ args = parser.parse_args()
 scheduler = Scheduler()
 scheduler.assign_config_by_path(args.s, args.b)
 print("Config loaded")
-
 
 scheduler.install_config()
 scheduler.initialize()
