9def2de5a0c0e1da9a9682b3ed110a7533b8edca
jyuan24 <jyan24@mit.edu>
2024-07-16 07:27:36 +0000

    configs work for fed weight non iid

--- src/configs/algo_config.py
+++ src/configs/algo_config.py
@@ -21,11 +21,50 @@ traditional_fl = {
     "exp_id": 10,
     "exp_type": "iid_clients_federated",
     # Learning setup
-    "epochs": 1000,
+    "epochs": 50,
     "model": "resnet34",
     "model_lr": 3e-4,
     "batch_size": 256,
     "exp_keys": [],
 }
 
-current_config = traditional_fl
+fedweight = {
+    "algo": "fedweight",
+    "exp_id": "test2",
+    "num_rep": 1,
+    "load_existing": False,
+
+    # Dataset params 
+    #"test_samples_per_class": 300,
+    #"test_samples_per_client": 400, # Only for non_iid test distribution
+    # "support" : get_sliding_window_support(num_clients=NUM_CLIENT, num_classes=10, num_classes_per_client=4),
+
+    # Clients selection
+    "target_clients": 1,
+    "similarity": "CosineSimilarity", #"EuclideanDistance", "CosineSimilarity", 
+    #"community_type": "dataset",
+    "with_sim_consensus": True,
+    
+    # Learning setup
+    "rounds": 200, 
+    "epochs_per_round": 5,
+    "warmup_epochs": 50,
+    "model": "resnet10",
+    "local_train_after_aggr" : True,
+    # "pretrained": True,
+    # "train_only_fc": True,
+    "model_lr": 1e-4, 
+    "batch_size": 16,
+    
+    # Knowledge transfer params
+    "average_last_layer": True,
+    "mask_finetune_last_layer": False,
+    #"own_aggr_weight": 0.3,
+    #"aggr_weight_strategy": "linear",
+
+    # params for model
+    "position": 0, 
+    "exp_keys": []
+}
+
+current_config = fedweight
--- src/configs/sys_config.py
+++ src/configs/sys_config.py
@@ -1,6 +1,8 @@
 # System Configuration
 # TODO: Set up multiple non-iid configurations here. The goal of a separate system config
 # is to simulate different real-world scenarios without changing the algorithm configuration.
+
+
 system_config = {
     "num_users": 3,
     "experiment_path": "./experiments/",
@@ -12,7 +14,7 @@ system_config = {
     # The device_ids dictionary depicts the GPUs on which the nodes reside.
     # For a single-GPU environment, the config will look as follows (as it follows a 0-based indexing):
     # "device_ids": {"node_0": [0], "node_1": [0],"node_2": [0], "node_3": [0]}
-    "device_ids": {"node_0": [5], "node_1": [5],"node_2": [5], "node_3": [2]},
+    "device_ids": {"node_0": [0], "node_1": [0],"node_2": [0], "node_3": [0]},
     "samples_per_user": 500, #TODO: To model scenarios where different users have different number of samples
     # we need to make this a dictionary with user_id as key and number of samples as value
     "train_label_distribution": "iid",
