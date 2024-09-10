71409281ea9f163ef5ea8cbdab0ffe8da13d2616
Yanis Aït El Cadi <79212470+Yanisaec@users.noreply.github.com>
2024-09-10 03:05:11 +0200

    Done some type inting here and there (#75)

--- src/utils/comm_utils.py
+++ src/utils/comm_utils.py
@@ -1,23 +1,24 @@
 from mpi4py import MPI
+from typing import List, Optional, Any
 
 
 class CommUtils:
-    def __init__(self):
+    def __init__(self) -> None:
         self.comm = MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
         self.size = self.comm.Get_size()
 
-    def send_signal(self, dest, data, tag=None):
+    def send_signal(self, dest: int, data: Any, tag: Optional[int] = None) -> None:
         if tag is not None:
             self.comm.send(data, dest=dest, tag=tag)
         else:
             self.comm.send(data, dest=dest)
 
-    def send_signal_to_all_clients(self, client_ids, data, tag=None):
+    def send_signal_to_all_clients(self, client_ids: List[int], data: Any, tag: Optional[int] = None) -> None:
         for client_id in client_ids:
             self.send_signal(client_id, data, tag=tag)
 
-    def wait_for_signal(self, src, tag=None):
+    def wait_for_signal(self, src: int, tag: Optional[int] = None) -> Any:
         """
         Wait for a signal from a source node
         """
@@ -27,8 +28,10 @@ class CommUtils:
             recv_data = self.comm.recv(source=src)
         return recv_data
 
-    def wait_for_all_clients(self, client_ids, tag=None):
-        """ """
+    def wait_for_all_clients(self, client_ids: List[int], tag: Optional[int] = None) -> List[Any]:
+        """
+        Wait for signals from all client nodes
+        """
         data_list = []
         for client_id in client_ids:
             data = self.wait_for_signal(client_id, tag=tag)
