ae175752276b46cf0f5d8f85cfdde6bf09eb651b
Gordon Lichtstein <72274426+generic-account@users.noreply.github.com>
2024-09-13 20:51:41 -0400

    Fixed typos in util files and extended algos type hinting (#79)
    Co-authored-by: generic-account <Gordon Lichtstein>
    Co-authored-by: Abhishek Singh <abhishek.s14@iiits.in>

--- src/algos/fl_random.py
+++ src/algos/fl_random.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 class RandomTopology:
+    """Method docstring: Returns selected IDs based on some criteria."""
     def get_selected_ids(self, node_id, config, reprs_dict, communities):
         within_community_sampling = config.get("within_community_sampling", 1)
 
