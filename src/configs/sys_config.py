54342dbd434de0fc3081f2aaec43162a3907512b
Lint Action <lint-action@samuelmeuli.com>
2024-10-06 16:06:23 +0000

    Fix code style issues with Black

--- src/configs/sys_config.py
+++ src/configs/sys_config.py
@@ -159,9 +159,7 @@ mpi_system_config = {
 
 mpi_non_iid_sys_config = {
     "exp_id": "",
-    "comm": {
-        "type": "MPI"
-    },
+    "comm": {"type": "MPI"},
     "seed": 1,
     "num_users": 3,
     # "experiment_path": "./experiments/",
@@ -182,9 +180,7 @@ mpi_non_iid_sys_config = {
 L2C_users = 3
 mpi_L2C_sys_config = {
     "exp_id": "",
-    "comm": {
-        "type": "MPI"
-    },
+    "comm": {"type": "MPI"},
     "seed": 1,
     "num_users": 3,
     # "experiment_path": "./experiments/",
@@ -205,9 +201,7 @@ mpi_L2C_sys_config = {
 
 mpi_metaL2C_support_sys_config = {
     "exp_id": "",
-    "comm": {
-        "type": "MPI"
-    },
+    "comm": {"type": "MPI"},
     "seed": 1,
     "num_users": 3,
     # "experiment_path": "./experiments/",
@@ -229,9 +223,7 @@ mpi_metaL2C_support_sys_config = {
 
 mpi_digitfive_sys_config = {
     "exp_id": "",
-    "comm": {
-        "type": "MPI"
-    },
+    "comm": {"type": "MPI"},
     "seed": 1,
     "num_users": 3,
     "load_existing": False,
@@ -255,9 +247,7 @@ mpi_digitfive_sys_config = {
 swarm_users = 3
 mpi_domainnet_sys_config = {
     "exp_id": "",
-    "comm": {
-        "type": "MPI"
-    },
+    "comm": {"type": "MPI"},
     "seed": 1,
     "num_users": 3,
     "load_existing": False,
