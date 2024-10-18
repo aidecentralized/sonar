95c6d6ddbb1974972a2091488be6cacdd0aabf57
IcyBroom <icybroom@gmail.com>
2024-10-18 00:12:40 -0400

    Update fl.py
    Updated fl.py file to run plot time using tensorboard

--- src/algos/fl.py
+++ src/algos/fl.py
@@ -45,6 +45,9 @@ class FedAvgClient(BaseClient):
         self.log_utils.log_tb(
             f"train_accuracy/client{self.node_id}", avg_accuracy, round
         )
+        self.log_utils.log_tb(
+            f"time/client{self.node_id}", time_taken, round
+        )
 
     def local_test(self, **kwargs: Any):
         """
@@ -177,6 +180,7 @@ class FedAvgServer(BaseServer):
             loss, acc, time_taken = self.test()
             self.log_utils.log_tb("test_acc/clients", acc, round)
             self.log_utils.log_tb("test_loss/clients", loss, round)
+            self.log_utils.log_tb("time/clients", time_taken, round)
             self.log_utils.log_console(
                 "Round: {} test_acc:{:.4f}, test_loss:{:.4f}, time taken {:.2f} seconds".format(
                     round, acc, loss, time_taken
