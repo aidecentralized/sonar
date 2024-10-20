a185d8a8bc4513c98f2467ffa56722d8c91dd8a1
Gautam Jajoo <gautamjajoo1729@gmail.com>
2024-10-20 22:05:47 +0530

    fix domainnet target issue (#118)
    * update requirements.txt
    
    * update docs
    
    * add desc
    
    * fix domainnet data target
    
    * remove unnecessary comments
    
    * update algo_config.py
    
    ---------
    
    Co-authored-by: tremblerz <abhishek.s14@iiits.in>

--- src/configs/sys_config.py
+++ src/configs/sys_config.py
@@ -265,9 +265,13 @@ mpi_domainnet_sys_config: ConfigType = {
     "seed": 1,
     "num_collaborators": NUM_COLLABORATORS,
     "load_existing": False,
+    "device_ids": get_device_ids(num_users=swarm_users, gpus_available=[1, 2]),
+    # "algo": get_algo_configs(num_users=swarm_users, algo_configs=default_config_list),  # type: ignore
+    "algos": get_algo_configs(
+        num_users=swarm_users,
+        algo_configs=default_config_list,
+    ),  # type: ignore
     "dump_dir": DUMP_DIR,
-    "device_ids": get_device_ids(num_users=swarm_users, gpus_available=[3, 4]),
-    "algo": get_algo_configs(num_users=swarm_users, algo_configs=default_config_list),  # type: ignore
     # Dataset params
     "dset": get_domainnet_support(
         swarm_users
@@ -275,7 +279,7 @@ mpi_domainnet_sys_config: ConfigType = {
     "dpath": domainnet_dpath,  # wilds_dpath,#domainnet_dpath,
     "train_label_distribution": "iid",  # Either "iid", "shard" "support",
     "test_label_distribution": "iid",  # Either "iid" "support",
-    "samples_per_user": 32,
+    "samples_per_user": 500,
     "test_samples_per_class": 100,
     "community_type": "dataset",
     "exp_keys": [],
