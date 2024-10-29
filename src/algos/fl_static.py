bcde3d1ad17c45d3d296b1535c85899bc0f6a311
photonshi <shiyichuan915@gmail.com>
2024-10-29 23:08:18 +0000

    gia for flStatic - need to add support for multiple attackers

--- src/algos/fl_static.py
+++ src/algos/fl_static.py
@@ -9,6 +9,7 @@ import time
 from algos.base_class import BaseFedAvgClient
 from algos.topologies.collections import select_topology
 
+from utils.gias import gia_main
 
 class FedStaticNode(BaseFedAvgClient):
     """
@@ -21,12 +22,41 @@ class FedStaticNode(BaseFedAvgClient):
         super().__init__(config, comm_utils)
         self.topology = select_topology(config, self.node_id)
         self.topology.initialize()
+        if "gia" in config:
+            self.params_s = dict()
+            self.params_t = dict()
+
+            self.attack_start_round = 0
+            self.attack_end_round = 1
+
+            self.base_params = [key for key, _ in self.model.named_parameters()]
 
     def get_representation(self, **kwargs: Any) -> OrderedDict[str, torch.Tensor]:
         """
         Returns the model weights as representation.
         """
         return self.get_model_weights()
+    
+    def receive_attack_and_aggregate(self, neighbors: List]) -> None:
+        """
+        Receives updates, launch GIA attack, and aggregate
+        """
+        for neighbor_info in neighbors:
+            # Receive the attack from the neighbors
+            neighbor_id = neighbor_info["sender"]
+            neighbor_model = neighbor_info["model"]
+            neighbor_images = neighbor_info["images"]
+            neighbor_labels = neighbor_info["labels"]
+
+            if round == self.attack_start_round:
+                self.params_s[neighbor_id] = neighbor_model
+            elif round == self.attack_end_round:
+                self.params_t[neighbor_id] = neighbor_model
+
+                # attack 
+                p_s, p_t = self.params_s[neighbor_id], self.params_t[neighbor_id]
+                gia_main(p_s, p_t, self.base_params, self.model, neighbor_labels, neighbor_images, self.node_id)
+
 
     def run_protocol(self) -> None:
         """
@@ -41,6 +71,7 @@ class FedStaticNode(BaseFedAvgClient):
             )
         total_rounds = self.config["rounds"]
         epochs_per_round = self.config.get("epochs_per_round", 1)
+
         for it in range(start_round, total_rounds):
             # Train locally and send the representation to the server
             stats["train_loss"], stats["train_acc"], stats["train_time"] = self.local_train(
@@ -50,10 +81,18 @@ class FedStaticNode(BaseFedAvgClient):
 
             # Collect the representations from all other nodes from the server
             neighbors = self.topology.sample_neighbours(self.num_collaborators)
+
             # TODO: Log the neighbors
             stats["neighbors"] = neighbors
 
-            self.receive_and_aggregate(neighbors)
+            if "gia" in self.config:
+                if it < self.attack_start_round or it > self.attack_end_round:
+                    self.receive_and_aggregate(neighbors)
+
+                else:
+                    self.receive_attack_and_aggregate(neighbors)
+            else:
+                self.receive_and_aggregate(neighbors)
 
             stats["bytes_received"], stats["bytes_sent"] = self.comm_utils.get_comm_cost()
 
@@ -61,8 +100,6 @@ class FedStaticNode(BaseFedAvgClient):
             # Inside FedStaticNode.run_protocol()
             stats["test_loss"], stats["test_acc"] = self.local_test()
             self.log_metrics(stats=stats, iteration=it)
-
-
 
 class FedStaticServer(BaseFedAvgClient):
     """
