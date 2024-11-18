a818cbef96ff14fdd3e1d42edd3faf372f79f686
Kathryn Le <kathrynlle9@gmail.com>
2024-11-18 11:19:45 -0500

    made testing sys and algo configs

---
+++ src/configs/algo_config_test.py
@@ -0,0 +1,15 @@
+from utils.types import ConfigType
+
+fedstatic: ConfigType = {
+    # Collaboration setup
+    "algo": "fedstatic",
+    "topology": {"name": "watts_strogatz", "k": 3, "p": 0.2}, # type: ignore
+    "rounds": 5,
+
+    # Model parameters
+    "model": "resnet10",
+    "model_lr": 3e-4,
+    "batch_size": 256,
+}
+
+# default_config_list: List[ConfigType] = [fedstatic, fedstatic, fedstatic, fedstatic]
