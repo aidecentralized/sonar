b5b6953674e71b53a7bb69a431c36f948dc16e16
joyce-yuan <joyceyuan80@gmail.com>
2025-03-11 18:20:06 +0000

    malicious configs and experiments script

---
+++ src/configs/algo_config_malicious.py
@@ -0,0 +1,334 @@
+from typing import Dict, List
+from .malicious_config import malicious_config_list
+import random
+from utils.types import ConfigType
+
+
+def get_malicious_types(malicious_config_list: List[ConfigType]) -> Dict[str, str]:
+    """
+    Assign a random malicious type to a single node.
+    """
+    malicious_type = random.choice(malicious_config_list)
+    return malicious_type # type: ignore
+
+
+# Algorithm Configuration
+
+iid_dispfl_clients_new: ConfigType = {
+    "algo": "dispfl",
+    "exp_type": "iid_dispfl",
+    "neighbors": 2,
+    "active_rate": 0.8,
+    "dense_ratio": 0.5,
+    "erk_power_scale": 1,
+    "anneal_factor": 0.5,
+    "epochs": 1000,
+    "model": "resnet34",
+    "model_lr": 3e-4,
+    "batch_size": 128,
+}
+
+traditional_fl: ConfigType = {
+    # Collaboration setup
+    "algo": "fedavg",
+    "rounds": 2,
+
+    # Model parameters
+    "model": "resnet10",
+    "model_lr": 3e-4,
+    "batch_size": 256,
+}
+
+test_fl_inversion: ConfigType = {
+    # Collaboration setup
+    "algo": "fedavg",
+    "rounds": 5,
+    "optimizer": "sgd",
+    # Model parameters
+    "model": "resnet10",
+    "model_lr": 3e-4,
+    # "batch_size": 256,
+    "gia": True,
+}
+
+fedweight: ConfigType = {
+    "algo": "fedweight",
+    "num_rep": 1,
+    # Client selection
+    "target_users": 3,
+    "similarity": "CosineSimilarity",  # "EuclideanDistance", "CosineSimilarity",
+    # "community_type": "dataset",
+    "with_sim_consensus": True,
+    # Learning setup
+    "rounds": 10,
+    "epochs_per_round": 5,
+    "warmup_epochs": 50,
+    "model": "resnet10",
+    "local_train_after_aggr": True,
+    # "pretrained": True,
+    # "train_only_fc": True,
+    "model_lr": 1e-4,
+    "batch_size": 16,
+    # Knowledge transfer params
+    "average_last_layer": True,
+    "mask_finetune_last_layer": False,
+    # params for model
+    "position": 0,
+}
+
+defkt: ConfigType = {
+    "algo": "defkt",
+    "central_client": 1,
+    "mask_last_layer": False,
+    "fine_tune_last_layer": False,
+    "epochs_per_round": 5,
+    "rounds": 10,
+    "epochs": 10,
+    "model": "resnet10",
+    "model_lr": 1e-4,
+    "batch_size": 16,
+    "num_teachers": 1,
+    # params for model
+    "position": 0,
+    "inp_shape": [128, 3, 32, 32],  # This should be a List[int]
+}
+
+fedavg_object_detect: ConfigType = {
+    "algo": "fedavg",
+    "exp_type": "",
+    # Learning setup
+    "epochs": 10,
+    "model": "yolo",
+    "model_lr": 1e-5,
+    "batch_size": 8,
+}
+
+fediso: ConfigType = {
+    "algo": "fediso",
+    "num_rep": 1,
+    # Learning setup
+    "rounds": 100,
+    "epochs_per_round": 5,
+    "model": "resnet10",
+    "model_lr": 1e-4,
+    "batch_size": 16,
+    # params for model
+    "position": 0,
+}
+
+L2C_users: int = 3
+L2C: ConfigType = {
+    "algo": "l2c",
+    "sharing": "weights",
+    "alpha_lr": 0.1,
+    "alpha_weight_decay": 0.01,
+    # Clients selection
+    "target_users_before_T_0": 0,  # Only used if adapted_to_assumption True otherwise all users are kept
+    "target_users_after_T_0": round((L2C_users - 1) * 0.1),
+    "T_0": 10,  # round after which only target_users_after_T_0 peers are kept
+    "epochs_per_round": 5,
+    "warmup_epochs": 5,
+    "rounds": 210,
+    "model": "resnet10",
+    "average_last_layer": True,
+    "model_lr": 1e-4,
+    "batch_size": 32,
+    "weight_decay": 5e-4,
+    "adapted_to_assumption": False,
+    # params for model
+    "position": 0,
+    "inp_shape": [128, 3, 32, 32],  # This should be a List[int]
+}
+
+fedcentral: ConfigType = {
+    "algo": "centralized",
+    "mask_last_layer": False,
+    "fine_tune_last_layer": False,
+    "epochs_per_round": 5,
+    "rounds": 100,
+    "model": "resnet10",
+    "model_lr": 1e-4,
+    "batch_size": 16,
+    # params for model
+    "position": 0,
+    "inp_shape": [128, 3, 32, 32],
+}
+
+fedval: ConfigType = {
+    "algo": "fedval",
+    "num_rep": 1,
+    # Clients selection
+    "selection_strategy": "highest",  # lowest,
+    "target_users_before_T_0": 1,
+    "target_users_after_T_0": 1,
+    "T_0": 400,  # round after which only target_users_after_T_0 peers are kept
+    "community_type": None,  # "dataset",
+    # Learning setup
+    "rounds": 200,
+    "epochs_per_round": 5,
+    "model": "resnet10",
+    "local_train_after_aggr": False,
+    "model_lr": 1e-4,
+    "batch_size": 16,
+    # Knowledge transfer params
+    "average_last_layer": True,
+    "mask_finetune_last_layer": False,
+    # params for model
+    "position": 0,
+}
+
+swarm_users: int = 3
+swarm: ConfigType = {
+    "algo": "swarm",
+    "num_rep": 1,
+    # Clients selection
+    "target_users": 2,
+    "similarity": "CosineSimilarity",  # "EuclideanDistance", "CosineSimilarity",
+    "with_sim_consensus": True,
+    # Learning setup
+    "epochs": 210,
+    "rounds": 210,
+    "epochs_per_round": 5,
+    "model": "resnet10",
+    "local_train_after_aggr": True,
+    "model_lr": 1e-4,
+    "batch_size": 16,
+    # Knowledge transfer params
+    "average_last_layer": True,
+    "mask_finetune_last_layer": False,
+    # params for model
+    "position": 0,
+}
+
+fedstatic: ConfigType = {
+    # Collaboration setup
+    "algo": "fedstatic",
+    "topology": {"name": "ring"}, # type: ignore
+    "rounds": 200,
+    # Model parameters
+    "optimizer": "sgd", # TODO comment out for real training
+    "model": "resnet10",
+    "model_lr": 0.1, # 3e-4,
+    "batch_size": 256,
+}
+
+swift: ConfigType = {
+    # Collaboration setup
+    "algo": "swift",
+    "topology": {"name": "watts_strogatz", "k": 3, "p": 0.2}, # type: ignore
+    "rounds": 20,
+
+    # Model parameters
+    "model": "resnet10",
+    "model_lr": 3e-4,
+    "batch_size": 256,
+}
+
+fed_dynamic_weights: ConfigType = {
+    # Collaboration setup
+    "algo": "feddynamic",
+    # comparison describes the metric or algorithm used to compare the weights of the models
+    # sampling describes the method used to sample the neighbors after the comparison
+    "topology": {"comparison": "weights_l2", "sampling": "closest"}, # type: ignore
+    "rounds": 200,
+
+    # Model parameters
+    "optimizer": "sgd",
+    "model": "resnet10",
+    "model_lr": 0.1,
+    "batch_size": 256,
+}
+
+fed_dynamic_loss: ConfigType = {
+    # Collaboration setup
+    "algo": "feddynamic",
+    "topology": {"comparison": "loss", "sampling": "closest"}, # type: ignore
+    "rounds": 20,
+
+    # Model parameters
+    "model": "resnet6",
+    "model_lr": 3e-4,
+    "batch_size": 256,
+}
+
+fedavgpush: ConfigType = {
+    # Collaboration setup
+    "algo": "fedavgpush",
+    "rounds": 2,
+
+    # Model parameters
+    "model": "resnet10",
+    "model_lr": 3e-4,
+    "batch_size": 256,
+}
+
+metaL2C_cifar10: ConfigType = {
+    "algo": "metal2c",
+    "sharing": "weights",  # "updates"
+    # Client selection
+    "target_users_before_T_0": 0,
+    "target_users_after_T_0": 1,
+    "K_0": 0,  # number of peers to keep as neighbors at T_0 (!) inverse that in L2C paper
+    "T_0": 250,  # round after wich only K_0 peers are kept
+    "alpha_lr": 0.1,
+    "alpha_weight_decay": 0.01,
+    "epochs_per_round": 5,
+    "rounds": 3,
+    "model": "resnet18",
+    "average_last_layer": False,
+    "model_lr": 1e-4,
+    "batch_size": 64,
+    "optimizer": "sgd",
+    "weight_decay": 5e-4,
+    # params for model
+    "position": 0,
+    "inp_shape": [128, 3, 32, 32],
+}
+
+
+# Malicious Algorithm Configuration
+malicious_traditional_model_update_attack: ConfigType = {
+    **traditional_fl,
+    **malicious_config_list["bad_weights"],
+}
+
+malicious_traditional_data_poisoning_attack: ConfigType = {
+    **traditional_fl,
+    **malicious_config_list["data_poisoning"],
+}
+
+malicious_traditional_model_poisoning_attack: ConfigType = {
+    **traditional_fl,
+    **malicious_config_list["backdoor_attack"],
+}
+
+
+
+# List of algorithm configurations
+algo_config_list: List[ConfigType] = [
+    iid_dispfl_clients_new,
+    traditional_fl,
+    malicious_traditional_data_poisoning_attack,
+    malicious_traditional_model_poisoning_attack,
+    malicious_traditional_model_update_attack,
+    fedweight,
+    defkt,
+    fedavg_object_detect,
+    fediso,
+    L2C,
+    fedcentral,
+    fedval,
+    swarm,
+    fedstatic,
+    metaL2C_cifar10,
+]
+
+# Malicious List of algorithm configurations
+malicious_algo_config_list: List[ConfigType] = [
+    fedstatic,
+    malicious_traditional_data_poisoning_attack,
+    malicious_traditional_model_poisoning_attack,
+    malicious_traditional_model_update_attack,
+]
+
+default_config_list: List[ConfigType] = malicious_algo_config_list
---
+++ src/run_malicious_experiments.py
@@ -0,0 +1,148 @@
+"""
+Given a set of experiment keys to run,
+this module writes the config files for each experiment key
+and runs the main.py script for each experiment
+"""
+
+import argparse
+import subprocess
+from typing import List
+
+from utils.types import ConfigType
+from utils.config_utils import process_config
+from utils.post_hoc_plot_utils2 import aggregate_metrics_across_users, plot_all_metrics
+
+from configs.sys_config import get_algo_configs, get_device_ids
+from configs.algo_config import fedstatic
+from configs.malicious_config import malicious_config_list
+from configs.sys_config import grpc_system_config
+import socket
+import time
+
+# Get the hostname
+hostname = socket.gethostname()
+superhost_name = "" # Fill in the superhost name
+full_hostname = "" # Fill in the full hostname
+
+post_hoc_plot: bool = True
+
+algo_to_algo_index = {
+    "data_poisoning": 0,
+    "gradient_attack": 1,
+    "backdoor_attack": 2,
+    "bad_weights": 3,
+    "sign_flip": 4,
+    "label_flip": 5,
+}
+
+# for each experiment key, write the modifications to the config file
+gpu_ids = [0, 1, 2, 3, 4, 5, 6, 7]
+exp_dict = {}
+num_nodes = 36
+for num_collaborators in [num_nodes, 1]:
+    for algo_name, algo_index in algo_to_algo_index.items():
+        for topo in ["ring", "torus", "fully_connected", "erdos_renyi"]:
+            for m in [0, 1, 4]:
+                topo_config = {"name": topo}
+                if topo == "erdos_renyi":
+                    topo_config["p"] = 0.13
+                exp_dict[f"topo_{topo}x{algo_name}_{m}_malicious_{num_collaborators}_colab_3_5"] = {
+                    "algo_config": fedstatic,
+                    "sys_config": grpc_system_config,
+                    "malicious_config": malicious_config_list[algo_name],
+                    "num_malicious": m,
+                    "algo": {
+                        "topology": topo_config,
+                    },
+                    "sys": {
+                        "comm": {"type": "GRPC", "synchronous": True, "peer_ids": ["matlaber1.media.mit.edu:1112"]},
+                        "num_users": num_nodes,
+                        "num_collaborators": num_collaborators,
+                        "samples_per_user": 50000 // num_nodes,
+                        "seed": 2,
+                        "assign_based_on_host": True,
+                    },
+                }
+
+# parse the arguments
+parser = argparse.ArgumentParser(description="host address of the nodes")
+
+args = parser.parse_args()
+
+skip = True
+for exp_id, exp_config in exp_dict.items():
+    if skip:
+        skip = False
+        continue
+    print(f"Running experiment {exp_config}")
+    # update the algo config with config settings
+    base_algo_config = exp_config["algo_config"].copy()
+    base_algo_config.update(exp_config["algo"])
+
+    # update the sys config with config settings
+    base_sys_config = exp_config["sys_config"].copy()
+    base_sys_config.update(exp_config["sys"])
+
+    # update the malicious config with config settings
+    base_malicious_config = exp_config["malicious_config"].copy()
+    base_malicious_config.update(base_algo_config)
+
+    # set up the full config file by combining the algo and sys config
+    n: int = base_sys_config["num_users"]
+    seed: int = base_sys_config["seed"]
+    m: int = exp_config["num_malicious"]
+    base_sys_config["algos"] = get_algo_configs(num_users=n, algo_configs=[base_algo_config, base_malicious_config], seed=seed, assignment_method="distribution", distribution={0: n-m, 1: m})
+    base_sys_config["device_ids"] = get_device_ids(n, gpu_ids)
+
+    full_config = base_sys_config.copy()
+    full_config["exp_id"] = exp_id
+
+    # write the config file as python file configs/temp_config.py
+    temp_config_path = "./configs/temp_config.py"
+    with open(temp_config_path, "w") as f:
+        f.write("current_config = ")
+        f.write(str(full_config))
+
+    superprocess = None
+    all_processes = []
+
+    # start the supernode
+    if hostname == superhost_name:
+        print("Starting supernode")
+        supernode_command: List[str] = ["python", "main.py", "-host", full_hostname, "-super", "true", "-s", temp_config_path]
+        superprocess = subprocess.Popen(supernode_command)
+    else:
+        print("Waiting for supernode to start")
+    time.sleep(10)
+
+    # start the nodes
+    command_list: List[str] = ["python", "main.py", "-host", full_hostname, "-s", temp_config_path]
+    for i in range(num_nodes):
+        print(f"Starting process for user {i} exp {exp_id}")
+        # start a Popen process
+        all_processes.append(subprocess.Popen(command_list))
+
+    # once the experiment is done, run the next experiment
+    # Wait for the supernode process to finish
+    if superprocess:
+        superprocess.wait()
+    else:
+        # wait for all the processes to finish
+        for process in all_processes:
+            process.wait()
+        # wait for 5 more minutes
+        print("Processes done, waiting for 5 minutes")
+        time.sleep(300)
+
+    # run the post-hoc analysis
+    if post_hoc_plot and superprocess is not None:
+        full_config = process_config(full_config) # this populates the results path
+        logs_dir = full_config["results_path"] + '/logs/'
+
+        # aggregate metrics across all users
+        aggregate_metrics_across_users(logs_dir)
+        # plot all metrics
+        plot_all_metrics(logs_dir)
+
+    # Continue with the next set of commands after supernode finishes
+    print(f"Supernode process {exp_id} finished. Proceeding to next set of commands.")
