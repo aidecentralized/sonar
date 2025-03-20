9780be73e17ca3f2a614c1248a8e9344a7da413b
joyce-yuan <joyceyuan80@gmail.com>
2025-03-20 20:06:18 +0000

    vite hosting changed

--- src/browser_client/vite.config.js
+++ src/browser_client/vite.config.js
@@ -4,5 +4,6 @@ export default {
     host: '0.0.0.0', // Bind to all network interfaces
     port: process.env.PORT || 10000, // Use Render's provided port
     strictPort: true // Ensures Vite fails if the port is unavailable
+    allowedHosts: ['.onrender.com'], // Allow all Render subdomains
   }
 }
