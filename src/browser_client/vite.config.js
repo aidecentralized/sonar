e4f6dd84160174ed53e256d462933dd743158a5b
joyce-yuan <joyceyuan80@gmail.com>
2025-03-20 20:03:12 +0000

    for hosting

--- src/browser_client/vite.config.js
+++ src/browser_client/vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
 export default {
-    server: {
-      open: true // automatically open browser on start
-    }
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: process.env.PORT || 10000, // Use Render's provided port
+    strictPort: true // Ensures Vite fails if the port is unavailable
   }
+}
